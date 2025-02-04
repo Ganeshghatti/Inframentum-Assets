9e98e16576c4303f92ab7a25df1570190b080185
Vijay Joshi <bhushanjoshi1714@gmail.com>
2025-02-04 20:17:06 +0530

    Browser Routes

--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import ScrollToTop from "../components/ScrollToTop";
+import ScrollToTop from "../Components/ScrollToTop";
 
 export default function Layout() {
   return (
